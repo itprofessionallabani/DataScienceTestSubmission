2ab6e95b1fedac31bdb9d6a87140cd66b7ec9ea7
itprofessionallabani <162355246+itprofessionallabani@users.noreply.github.com>
2024-03-05 14:13:54 -0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+### Task 2: Insert a new code cell below and use this **expression** <code>(20+5)*4</code> and **execute** it.
+<details><summary>Click here for the solution</summary>
+
+```python
+(20+5)*4
+
+```
+
+</details>
